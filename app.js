3f9e541b0409be729d556e960076184eea97c806
ananth astaputra <63888555+ananthastaputra@users.noreply.github.com>
2021-04-01 13:48:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const mongoose  = require('mongoose')
+const PORT = process.env.PORT || 5000
+const {MONGOURI}=require("./keys")
+
+
+
+require('./models/user')
+require('./models/post')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
+// app.use(require('./routes/user'))
+
+
+
+
+
+
+
+mongoose.connect(MONGOURI,{
+   useNewUrlParser:true,
+   useUnifiedTopology: true
+
+})
+
+mongoose.connection.on("connected",()=>{
+   console.log("connected to mongo")
+
+})
+
+mongoose.connection.on("error",(err)=>{
+   console.log("error in connecting",err)
+})
+
+
+
+
+
+app.listen(PORT,()=>{
+   console.log("server is running on",PORT)
+})
---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports={
+   MONGOURI:"mongodb+srv://ananth301:ananth1996@cluster0.jwehr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+   JWT_SECRET:"ananth1996"
+}
